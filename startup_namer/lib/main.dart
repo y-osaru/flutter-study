dad658116740c9316a5641ba646ebe38da1df798
y-osaru <osaru.tus.1410049@gmail.com>
2019-08-08 23:14:55 +0900

    Flutter チュートリアル part2実装(色々TODOにやってみたい事メモった)

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,11 +1,8 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.pinkAccent,
+      ),
       home: RandomWords(),
     );
   }
@@ -25,7 +25,9 @@ class RandomWords extends StatefulWidget{
 }
 
 class RandomWordsState extends State<RandomWords>{
+  //Dartは型を書いても書かなくてもOK？
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize:18.0);
 
   Widget _builSuggestions(){
@@ -33,8 +35,6 @@ class RandomWordsState extends State<RandomWords>{
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context,i){
-        print("i:" + i.toString());
-
         //iが奇数の時には仕切り線を表示
         if(i.isOdd) return Divider();
 
@@ -43,21 +43,31 @@ class RandomWordsState extends State<RandomWords>{
           _suggestions.addAll(generateWordPairs().take(10));
         }
         
-        //Q:このListViewの実行頻度ってどうなってるの？
-        //→要素が描画されたタイミングで実行される？
-        //→もしかして気にしたら負けなやつ？(内部で良しなにやってる？)
-        print("index:" + index.toString());
-        print("list length:" + _suggestions.length.toString());
-        print("time:"+DateTime.now().toString());
-
         return _buildRow(_suggestions[index]);
       },
     );
   }
 
   Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title:Text(pair.asPascalCase,style:_biggerFont),
+      //trailingはListTileの右側に出るWidget
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      //TODO:このタップをIconに持たせるには？→IconButton？
+      onTap: (){
+        //setState()はFWが変更を検知したら呼ばれる
+        setState((){
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -65,9 +75,40 @@ class RandomWordsState extends State<RandomWords>{
   Widget build(BuildContext context){
     return Scaffold(
       appBar:AppBar(
-        title:Text('Startup Name Generator')
+        title:Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon:Icon(Icons.list),onPressed: _pushSaved,)
+        ],
       ),
       body: _builSuggestions(),
+    );
+  }
+
+  void _pushSaved(){
+    //Widgetをpushする。
+    //TODO:ここの部分は別のStateに分ける事で分離になる？_savedをどう渡すのかが鍵かな。
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context){
+          final tiles = _saved.map(
+            (WordPair pair){
+              return ListTile(
+                title:Text(pair.asPascalCase,style: _biggerFont,),
+              );
+          });
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles:tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title:Text('Saved Suggestions'),
+            ),
+            body:ListView(children: divided,),
+          );
+        },
+      ),
     );
   }
 }
